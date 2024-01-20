578fcca9badaedad0c5f3c5f4a2e5e870baad761
reubence <reuben.rapose@gmail.com>
2024-01-20 19:42:53 +0530

    Change default tab value to "unread" in Mail component

--- components/mail/mail.tsx
+++ components/mail/mail.tsx
@@ -170,7 +170,7 @@ export function Mail({
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-          <Tabs defaultValue="all">
+          <Tabs defaultValue="unread">
             <div className="flex items-center px-4 py-2">
               {/* <h1 className="text-xl font-bold">Inbox</h1> */}
               <TabsList className="ml-auto">
